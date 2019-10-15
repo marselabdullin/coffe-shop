de541c7a6112706f0bfad42358763af987c0b4cd
marselabdullin <abdmars@mail.ru>
2019-10-15 21:20:13 +0300

    Updates

---
+++ precache-manifest.418773df865331624f0555a61ec4c8eb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d7b1cd1688f8d11c05ab4427881783",
+    "url": "/coffe-shop/index.html"
+  },
+  {
+    "revision": "4c3dc8fbcb80095623ee",
+    "url": "/coffe-shop/static/css/main.7f28bd41.chunk.css"
+  },
+  {
+    "revision": "1d1c8887380ae00b845a",
+    "url": "/coffe-shop/static/js/2.38f63a63.chunk.js"
+  },
+  {
+    "revision": "4c3dc8fbcb80095623ee",
+    "url": "/coffe-shop/static/js/main.fdc119f5.chunk.js"
+  },
+  {
+    "revision": "542f0c7706be428a7ef5",
+    "url": "/coffe-shop/static/js/runtime-main.fa51763a.js"
+  },
+  {
+    "revision": "4591afda4853c5f18d118ea844041bf4",
+    "url": "/coffe-shop/static/media/Beans_logo.4591afda.svg"
+  },
+  {
+    "revision": "120a377caff7231b3ac5acc54139d9b7",
+    "url": "/coffe-shop/static/media/Beans_logo_dark.120a377c.svg"
+  },
+  {
+    "revision": "7413c67d7da6e7c04269d1a4079069b2",
+    "url": "/coffe-shop/static/media/Coffee_bg.7413c67d.jpg"
+  },
+  {
+    "revision": "6e5b2328153725886030e90aa399e5b6",
+    "url": "/coffe-shop/static/media/Logo.6e5b2328.svg"
+  },
+  {
+    "revision": "8c7fe0916425b04a3760438dc40e4e35",
+    "url": "/coffe-shop/static/media/Logo_black.8c7fe091.svg"
+  },
+  {
+    "revision": "2fdcd636b72b8452c9d51e5635185ddb",
+    "url": "/coffe-shop/static/media/Main_bg.2fdcd636.jpg"
+  },
+  {
+    "revision": "6e7d1e4eda3c4931aa8e6c76b0b6f465",
+    "url": "/coffe-shop/static/media/coffee_girl.6e7d1e4e.jpg"
+  },
+  {
+    "revision": "ac0d5470a57aaf1d935d3b8ad82db51d",
+    "url": "/coffe-shop/static/media/goods.ac0d5470.jpg"
+  },
+  {
+    "revision": "b69bd070042bf27627c518c04add96f2",
+    "url": "/coffe-shop/static/media/paper.b69bd070.jpg"
+  }
+]);
